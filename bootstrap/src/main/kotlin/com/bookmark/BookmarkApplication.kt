559b58ac95552073c59669756fdd4a7ec1746493
Avinash <>
2020-09-05 14:45:06 +0530

    build(bootstrap): test heroku deployment

--- bootstrap/src/main/kotlin/com/bookmark/BookmarkApplication.kt
+++ bootstrap/src/main/kotlin/com/bookmark/BookmarkApplication.kt
@@ -2,10 +2,19 @@ package com.bookmark
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseBody
 
-
+@Controller
 @SpringBootApplication
-open class BookmarkApplication
+open class BookmarkApplication{
+    @RequestMapping("/")
+    @ResponseBody
+    fun home(): String {
+        return "Hello World!";
+    }
+}
 
 fun main(args: Array<String>) {
     println("application started")
