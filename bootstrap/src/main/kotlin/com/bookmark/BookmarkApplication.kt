c71273a4bc229cb0ffc2666a61e8109d8d8ee997
Avinash <>
2020-09-06 21:49:04 +0530

    build(bookmark): added postgre JPA congif

--- bootstrap/src/main/kotlin/com/bookmark/BookmarkApplication.kt
+++ bootstrap/src/main/kotlin/com/bookmark/BookmarkApplication.kt
@@ -2,21 +2,10 @@ package com.bookmark
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.stereotype.Controller
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.ResponseBody
 
-@Controller
 @SpringBootApplication
-open class BookmarkApplication{
-    @RequestMapping("/")
-    @ResponseBody
-    fun home(): String {
-        return "Hello World!";
-    }
-}
+open class BookmarkApplication
 
 fun main(args: Array<String>) {
-    println("application started")
     runApplication<BookmarkApplication>(*args)
 }
