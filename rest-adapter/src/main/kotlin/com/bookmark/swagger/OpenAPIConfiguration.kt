6994faff30c0f958e8894e03ab31c07872976aa1
Avinash <>
2020-09-15 14:56:34 +0530

    feat(bookmark): swagger - API definitions and user details

---
+++ rest-adapter/src/main/kotlin/com/bookmark/swagger/OpenAPIConfiguration.kt
@@ -0,0 +1,14 @@
+package com.bookmark.swagger
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition
+import io.swagger.v3.oas.annotations.info.Contact
+import io.swagger.v3.oas.annotations.info.Info
+
+
+@OpenAPIDefinition(info = Info(title = "Create the short urls and bookmarks and grouping it",
+        description = "User should generate short URLs which will expire " +
+                "after a standard default timespan " +
+                "and able to group cards in terms of tribes, feature teams, platforms or application",
+        contact = Contact(name = "Avinasha NV", url = "https://avinash1604.github.io/bookmarks",
+                email = "avinashnv2@gmail.com")))
+class OpenAPIConfiguration
